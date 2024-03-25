ca3dc898ea25ca2b410fee8bc64a2870141390e8
lownoble <1085684204@qq.com>
2024-03-25 11:48:58 +0800

    rm useless dir

--- odroid/Hyleg/hyleg_guide/src/FSM/FSMState.cpp
+++
@@ -1,7 +0,0 @@
-#include "FSM/FSMState.h"
-
-FSMState::FSMState(CtrlComponents *ctrlComp, FSMStateName stateName, std::string stateNameString)
-    :_ctrlComp(ctrlComp), _stateName(stateName), _stateNameString(stateNameString){
-        _lowCmd = ctrlComp -> lowCmd;
-        _lowState = _ctrlComp -> lowstate;
-}
